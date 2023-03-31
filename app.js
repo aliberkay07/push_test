e5b5de602f43cece7d889ac7965d4fd7965bbd91
Ali Berkay <aliberkaycakiroglu@gmail.com>
2023-03-31 12:11:16 +0300

    fist pushhh

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿window.addEventListener('load', async () =>{
+    const aboneol = document.querySelector('#aboneol')
+
+    const sw = await navigator.serviceWorker.register("./sw.js");
+    console.log("serbice worker =>", sw);
+    aboneol.addEventListener('click', async () => {
+        
+        const serviceWorker = await navigator.serviceWorker.ready;
+        const cliendID = await serviceWorker.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: "BAJofqCfr_ztf2ZXJYa7rz1XEjEirEKgP1dJ9Mln5PslYR-eJhnTLALWYaZyHrTMxTiYr7uakXQvZ-WxOr40Q2M",
+
+        });
+
+        console.log(cliendID);
+        console.log(JSON.stringify(cliendID));
+
+
+    })
+
+
+})
---
+++ sw.js
@@ -0,0 +1,10 @@
+﻿self.addEventListener("pash", (e) => {
+    const config = {
+        body: "mesaj içeriği",
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: "1"
+        }
+    }
+    e.waitUntil(self.registration.showNotification("Yeni makale eklendi", config));
+})
